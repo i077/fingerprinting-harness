efa4567768e5df1628b3e396cc216258f34caeaa
Zubair Mukhi <zubair.mukhi@gmail.com>
2021-04-15 23:43:35 -0400

    more changes

--- zap.py
+++ zap.py
@@ -1,14 +1,20 @@
 import subprocess
 import requests
+import os
+from dotenv import load_dotenv
 
-def enable_zap(site: str, base_path: str) -> subprocess.Popen:
+working_directory = os.getcwd()
+add_to_env = os.path.join(working_directory, '.env')
+load_dotenv(add_to_env)
+
+API = os.getenv('API')
+
+def enable_zap(site: str, base_path: str):
     path = f'{base_path}/{site}/{site}'
-    res = subprocess.Popen(args=['zap.bat', "-daemon", '-newsession', path])
-    return res
+    subprocess.run(args=['zap', "-daemon", '-newsession', path, '-config', f'api.key={API}'])
 
-def kill_zap(process: subprocess.Popen):
-    process.kill()
+def kill_zap():
+    requests.get(f"http://localhost:8080/JSON/core/action/shutdown/?apikey={API}")
 
 if __name__ == '__main__':
-    proc = enable_zap('cnn.com')
-    print(proc.pid)
+    enable_zap('cnn.com', '/home/zubair/zap/cnn2')
