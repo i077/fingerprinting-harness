041f1c12935f688a0ccfca12064fe8094f90c166
Imran Hossain <hi@imranh.xyz>
2021-04-24 18:35:27 -0400

    Save list of requests made to JSON file

--- run_harness.py
+++ run_harness.py
@@ -1,6 +1,11 @@
-from typing import List
+import json
+from typing import Dict, List
 import typer
 from selenium.webdriver import Firefox, FirefoxOptions
+
+GET_REQ_JS = """
+var performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {}; var network = performance.getEntries() || {}; return network;
+"""
 
 
 def main(
@@ -15,10 +20,32 @@ def main(
 
     # Get list of sites
     sites: List[str]
+    requests_by_site: Dict[str, List] = {}
     with open("sites.txt", "r") as f:
         lines = f.readlines()
         sites = [l.strip() for l in lines]
     typer.secho(f"Found {len(sites)} site(s) to read.")
+
+    # Get list of requests for each site
+    for site in sites:
+        requests_by_site[site] = visit_site(driver, site)
+    typer.secho("Closing browser.")
+    driver.close()
+
+    # Save lists to a JSON file
+    with open("requests.json", "w") as f:
+        json.dump(requests_by_site, f)
+
+
+def visit_site(driver: Firefox, url: str):
+    """
+    Visit a site and log the requests the browser makes during the page load.
+    Returns a list of requests made.
+    """
+    typer.secho(f"Visiting {url}...")
+    driver.get(url)
+    requests_list: list = driver.execute_script(GET_REQ_JS)
+    return requests_list
 
 
 if __name__ == "__main__":
